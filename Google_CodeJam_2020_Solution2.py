1ffe681c0d2837d4790290a02d4c071912cee352
dbrincauus <55274543+dbrincauus@users.noreply.github.com>
2020-04-04 23:56:25 +0200

    Update Google_CodeJam_2020_Solution2.py

--- Google_CodeJam_2020_Solution2.py
+++ Google_CodeJam_2020_Solution2.py
@@ -5,25 +5,34 @@ while n < cases:
 	res += "Case #" + str((n+1)) + ": "
 
 	linea = input()
+	line = ""
 	for i in linea:
-		if res[len(res) - 1] == "0":
-			if i == "0":
-				res += "0"
-			elif i == "1":
-				res += "(1"
-		elif res[len(res) - 1] == "1":
-			if i == "0":
-				res += ")0"
-			elif i == "1":
-				res += "1"
-		else:
-			if i == "0":
-				res += "0"
-			elif i == "1":
-				res += "(1"
+		if i == "0":
+			line += "0"
+		if i == "1":
+			line += "(1)"
+		if i == "2":
+			line += "((2))"
+		if i == "3":
+			line += "(((3)))"
+		if i == "4":
+			line += "((((4))))"
+		if i == "5":
+			line += "(((((5)))))"
+		if i == "6":
+			line += "((((((6))))))"
+		if i == "7":
+			line += "(((((((7)))))))"
+		if i == "8":
+			line += "((((((((8))))))))"
+		if i == "9":
+			line += "(((((((((9)))))))))"
 
-	if res[len(res) - 1] == "1":
-		res += ")"
+	while ")(" in line:
+		line = line.replace(")(", "")
+
+	res += line
+
 
 	res += "\n"
 	n += 1
